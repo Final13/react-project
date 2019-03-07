c695c829a110e7a3cb7787e012cf539308897e46
Ivan Matskevich <faxtalk666@gmail.com>
2019-03-07 16:41:08 +0300

    refactoring

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -5,56 +5,55 @@ const validateRegisterInput = require('../validation/register');
 const validateLoginInput = require('../validation/login');
 const User = require('../models/User');
 
-const register = (req, res) => {
+const register = async (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
 
     if(!isValid) {
         return res.status(400).json(errors);
     }
 
-    User.findOne({
+    const user = await User.findOne({
         email: req.body.email
-    }).then(user => {
-        if(user) {
-            return res.status(400).json({
-                email: 'Email already exists'
-            });
-        }
-        else {
-            const avatar = gravatar.url(req.body.email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            });
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                role: req.body.email ==='faxtalk666@gmail.com' ? 'admin' : 'user',
-                avatar
-            });
-
-            bcrypt.genSalt(10, (err, salt) => {
-                if(err) console.error('There was an error', err);
-                else {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) console.error('There was an error', err);
-                        else {
-                            newUser.password = hash;
-                            newUser
-                                .save()
-                                .then(user => {
-                                    res.json(user)
-                                });
-                        }
-                    });
-                }
-            });
-        }
     });
+
+    if(user) {
+        return res.status(400).json({
+            email: 'Email already exists'
+        });
+    } else {
+        const avatar = gravatar.url(req.body.email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            role: req.body.email ==='faxtalk666@gmail.com' ? 'admin' : 'user',
+            avatar
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+            if(err) console.error('There was an error', err);
+            else {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) console.error('There was an error', err);
+                    else {
+                        newUser.password = hash;
+                        (async () => {
+                            const user = await newUser.save();
+                            res.json(user);
+                        })();
+                    }
+                });
+            }
+        });
+    }
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
 
     const { errors, isValid } = validateLoginInput(req.body);
 
@@ -64,39 +63,37 @@ const login = (req, res) => {
 
     const { email, password } = req.body;
 
-    User.findOne({email})
-        .then(user => {
-            if(!user) {
-                errors.email = 'User not found';
-                return res.status(404).json(errors);
-            }
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if(isMatch) {
-                        const payload = {
-                            id: user.id,
-                            name: user.name,
-                            avatar: user.avatar,
-                            role: user.role,
-                        };
-                        jwt.sign(payload, 'secret', {
-                            expiresIn: 3600
-                        }, (err, token) => {
-                            if(err) console.error('There is some error in token', err);
-                            else {
-                                res.json({
-                                    success: true,
-                                    token: `Bearer ${token}`
-                                });
-                            }
-                        });
-                    }
-                    else {
-                        errors.password = 'Incorrect Password';
-                        return res.status(400).json(errors);
-                    }
+    const user = await User.findOne({email});
+    if(!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(isMatch) {
+        const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+            role: user.role,
+        };
+        jwt.sign(payload, 'secret', {
+            expiresIn: 3600
+        }, (err, token) => {
+            if(err) console.error('There is some error in token', err);
+            else {
+                res.json({
+                    success: true,
+                    token: `Bearer ${token}`
                 });
+            }
         });
+    }
+    else {
+        errors.password = 'Incorrect Password';
+        return res.status(400).json(errors);
+    }
 };
 
 module.exports = {
