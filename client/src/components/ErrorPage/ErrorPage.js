8525f1522a025ac779d9a4838725b8c5a531f9a7
Ivan Matskevich <faxtalk666@gmail.com>
2019-04-18 12:46:37 +0300

    minor fixes

--- client/src/components/ErrorPage/ErrorPage.js
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styles from './ErrorPage.module.scss'
+import {Helmet} from "react-helmet";
 
 const ErrorPage = () => {
     return (
         <div className={styles.container}>
+            <Helmet>
+                <title>404 page | Brand Name</title>
+            </Helmet>
             <h1 className={`w-100 ${styles.header}`}>Page Not Found</h1>
             <p className={styles.paragraph}>
                 The page you are looking for can't be found.
